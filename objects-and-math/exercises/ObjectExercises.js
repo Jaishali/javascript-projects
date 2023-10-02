ff997161f59a136f8005e12cd9d96b34c703f334
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-10-02 11:00:57 -0500

    Object and math Exercises
    updated

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -1,22 +1,101 @@
+
+// After you have created the other object literals, add the astronautID property to each one.
+//Create an array to hold the animal objects.
+ //Print out the relevant information about each animal.
+// Start an animal race!
+
+//let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
 let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
 };
+for (item in superChimpOne) {
+   console.log(item + ", " + superChimpOne[item]);
+}
 
+/*function superChimpOne(arr){
+   let index = Math.floor(Math.random()*arr.length);
+   return arr[index];
+}
+for (i=0; i < 4; i++){
+   console.log(superChimpOne(crew));
+}*/
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
 };
+for (item in salamander) {
+   console.log(item + ", " + salamander[item]);
+}
+
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+for (item in superChimpTwo) {
+   console.log(item + ", " + superChimpTwo[item]);
+}
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+for (item in dog) {
+   console.log(item + ", " + dog[item]);
+}
+
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+for (item in waterBear) {
+   console.log(item + ", " + waterBear[item]);
+}
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
 
 
-// After you have created the other object literals, add the astronautID property to each one.
+function crewReports(animal){
+   species: "Galapagos Tortoise";
+   name: "Patricia";
+   weight: 800;
+   age: 85;
+};
+console.log(crewReports);
 
-// Create an array to hold the animal objects.
 
-// Print out the relevant information about each animal.
 
-// Start an animal race!
+function fitnessTest(candidates ){
+   let results = [], numSteps = [] , turns = [];
+   for (let i = 0; i < candidates.length; i++){
+       numSteps = 0;
+       turns = 0;
+       while(numSteps < 20){
+       numSteps += candidates[i].move();
+       turns++;
+       }
+       results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  };
+  console.log(fitnessTest.results);
