077be40cadf762e0092116832a643083304217af
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-10-02 20:20:18 -0500

    Objects and math studio
    updated

--- objects-and-math/studio/ObjectsStudio03.js
+++ objects-and-math/studio/ObjectsStudio03.js
@@ -1,9 +1,55 @@
+//Define a function to selsct a crew member by oxygen use and determine the canditate for the spacewalk
+function selectByOxygenUse(arr){
+  let canditate = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i].o2Used(1) < canditate.o2Used(1)){
+      canditate = arr[i];
+    }
+  }
+  return canditate;
+}
+
+//select a new candidate by oxygen use
+let selectedCandidateBonus = selectByOxygenUse(crew);
+console.log(oxygenExpended(selectedCandidateBonus));
+
+//code a helper function just to caculate the total mass
+function getTotalMass(arr){
+  return crewMass(arr) + ROCKET_MASS + safetySurplus(arr);
+}
+
+
+
+
+
+
 // Code your crewMass function here:
-
-
+function crewMass(arr){
+  let mass = 0;
+  for (let i= 0; i < arr.length; i++){
+    mass += arr[i].mass;
+  }
+return Math.round(mass * 10)/ 10
+}
+ const ROCKET_MASS = 75000;
 // Code your fuelRequired function here:
+function fuelRequired(arr){
+return getTotalMass(arr) * 9.5;
+}
 
+function safetySurplus(arr){
+  let surplus = 100 * arr.length;
+  for (let i=0; i<arr.length; i++){
+    if(arr[i].species === "cat" || arr[i].species === "dog"){
+      surplus += 100;
+    }
+  }
+  return surplus;
+}
 
+crew = [candidateA,candidateC,candidateE];
+
+console.log(`the mission has a launch mass of ${getTotalMass(crew)} kg and requires ${fuelRequired(crew)} kg of fuel.`);
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
 
@@ -50,5 +96,5 @@ let candidateA = {
    'astronautID':890
  };
  
- let crew = [candidateB,candidateD,candidateF];
+ // crew = [candidateB,candidateD,candidateF];
  
