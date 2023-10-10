19728853b2ef8bccfb0ea6d7abef11ac53dd114a
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-10-10 13:43:43 -0500

    unit -testing studio
    updated

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,20 @@
 
 let launchcode = {
-    organization = "nonprofit";
-  
+    organization : "nonprofit",
+    executiveDirector :"Jeff",
+    percentageCoolEmployees : "100",
+    programOffered :["Lc101", "LaunchCode Women+", "CoderCamp"],
+    launchcode: function(num){
+        if(num % 2 === 0){
+            return "Launch!";
+        }
+        else if(num % 3 === 0){
+            return "Code!";
+        }
+        else if (num % 5 === 0){
+            return "Rocks!";
+        }
+    }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,9 +4,47 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  test("organization is nonprofit ", function(){
-    let output = launchcode;
-    expect(output).toBe("Object")
+  test("testing launchcode", function(){
+    expect(launchcode.organization).toBe("nonprofit");
  });
+ test("check executiveDirector",function(){
+expect(launchcode.executiveDirector).toBe("Jeff");
+ });
+ test("check percentageCoolEmployees", function(){
+  expect(launchcode.percentageCoolEmployees).toBe("100");
+});
+test("check program",function(){
+  expect(launchcode.programOffered[0]).toBe("Lc101");
+  expect(launchcode.programOffered[1]).toBe("LaunchCode Women+");
+  expect(launchcode.programOffered[2]).toBe("CoderCamp");
+  expect(launchcode.programOffered.length).toBe(3);
+  });
  });
   
+describe("should have a method, launchcode, which", function(){
+test("should return 'Launch!' for numbers evenly divisible by only 2", function(){
+expect(launchcode.launchcode(2)).toBe("Launch!");
+});
+test("should return 'Code!' for numbers evenly divisible by only 3", function(){
+  expect(launchcode.launchcode(3)).toBe("Code!");
+});
+test("should return 'Rocks!' for numbers evenly divisible by only 5", function(){
+  expect(launchcode.launchcode(5)).toBe("Rocks!");
+});
+test("should return 'LaunchCode!' for numbers evenly divisible by 2 and 3",function(){
+expect(launchcode.launchcode(2, 3)).toBe('LaunchCode!');
+});
+test("should return 'Code Rocks!' for numbers evenly divisible by 3 and 5",function(){
+  expect(launchcode.launchcode(3, 5)).toBe('Code Rocks!');
+  });
+  test("should return 'Launch Rocks! (CRASH!!!!)' for numbers evenly divisible by 2 and 5",function(){
+    expect(launchcode.launchcode(2, 5)).toBe('Launch Rocks! (CRASH!!!!)');
+    });
+    test("should return 'LaunchCode Rocks!' for numbers evenly divisible by 2,3 and 5",function(){
+      expect(launchcode.launchcode(2, 3 ,5)).toBe('LaunchCode Rocks!');
+      });
+      test("should return 'Rutabagas! That doesnot't work.' for numbers evenly not divisible by 2,3 and 5",function(){
+        expect(launchcode.launchcode(2, 3 ,5)).toBe('Rutabagas! That doesnot work');
+        });
+        
+});
