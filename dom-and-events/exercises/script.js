df7b92ad38dd7f2daa0f5a9f6a3af3b9397021e8
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-10-29 15:03:30 -0500

    dom-and-events exercises
    updated

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,18 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function( event ) {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("click", function( event ) {
+        event.target.style.backgroundColor = "red";
+     });
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+     missionAbort.addEventListener("click", function( event ) {
+        paragraph.innerHTML = 'Are you sure you want to abort the mission?';
+     });
     
 }
 
